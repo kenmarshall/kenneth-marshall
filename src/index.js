a20929afa7b49e747411ff277cc2677d9065ca87
Kenneth Marshall <kdmarshall89@gmail.com>
2024-11-24 07:22:19 -0500

    first draft

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap'
+import './scss/styles.scss'
+import './js/scroll_interaction'
+
+if (module.hot) {
+  module.hot.accept();
+}
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js", // Entry point
+  output: {
+    filename: "bundle.js", // Output file
+    path: path.resolve(__dirname, "dist") // Output directory
+  },
+  mode: "development", // Set to 'production' for optimized builds
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist") // Directory to serve
+    },
+    compress: true, // Enable gzip compression
+    port: 9000, // Port to run the server
+    open: true, // Automatically open the browser
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)$/, 
+        type: 'asset/resource', 
+      },
+    ]
+  }
+};
